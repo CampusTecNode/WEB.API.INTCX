61254b6d41b38fe380354db71833ac3646ab0931
David-TP <dsolano@tiendapago.com>
2024-09-27 01:00:40 -0400

    First Commit. Express.js Proyect Created

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+const morgan = require('morgan');
+
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2)
+
+app.get('/', (req, res) => {    
+  res.json(
+      {
+          "Title": "Hola mundo"
+      }
+  );
+})
+
+app.listen(app.get('port'),()=>{
+  console.log(`Server listening on port ${app.get('port')}`);
+});
