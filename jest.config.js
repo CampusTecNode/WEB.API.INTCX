a14bce7a45a1ae0d5f607878d35f44bca72c6aa2
David-TP <dsolano@tiendapago.com>
2024-10-02 21:49:10 -0400

    Update ci-cd.yml and jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
     testEnvironment: 'node',
     verbose: true,
-}
+    
+    // Agrega los reporteros aquí
+    reporters: [
+      'default',
+      ['jest-junit', {
+        outputDirectory: './test-reports',  // Directorio donde se guardarán los reportes
+        outputName: 'junit.xml',            // Nombre del archivo de reporte
+      }]
+    ]
+  };
+  
