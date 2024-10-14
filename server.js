7ad935d9783a7295347041585a15258d47d47f5e
David-TP <dsolano@tiendapago.com>
2024-10-14 01:10:44 -0400

    Skiping seeder

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 3000;
 async function initializeApp() {
   try {
     // Sincroniza todos los modelos con la base de datos
-    await sequelize.sync({ force: true }); 
+    await sequelize.sync(); 
     console.log('Tablas creadas exitosamente.');
 
-    if (process.env.NODE_ENV === 'development') {
-      await seedDatabase();
-    }
+    // if (process.env.NODE_ENV === 'development') {
+    //   await seedDatabase();
+    // }
     
     app.listen(port, () => {
         console.log(`Servidor escuchando en el puerto ${port}`);
